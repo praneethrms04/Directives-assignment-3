bd16c2d1ee8885948cc250057ed5ee606567f012
PRANEETH KUMAR <100190528+praneethrms04@users.noreply.github.com>
2023-06-20 13:46:43 +0530

    Data binding concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Data-binding';
+  count: number = 0;
+  name = "hello"
+  city = "Puttaparthy"
+
+  increment() {
+    this.count += 1;
+  }
+
+  decrement() {
+    this.count -= 1;
+  }
+
+  reset() {
+    this.count = 0;
+  }
+
+  changeName(e: any) {
+     this.name = e.target.value;
+  }
+ 
+  updatecity (e: any){
+    this.city = e.target.value
+  }
+
 }
